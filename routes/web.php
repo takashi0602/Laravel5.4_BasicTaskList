94cef316a411ab1d7d1d383444ec34441cc34c54
Kazuki Tobita <kazukiti201@gmail.com>
2017-07-13 18:48:29 +0900

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/** Show Task Dashbord **/
+Route::get('/', function() {
+	$tasks = \App\Task::orderBy('created_at', 'asc')->get();
+
+	return view('task', [
+		'tasks' => $tasks	// このデータがtask.blade.phpで使えるようになる
+	]);
+});
+
+Route::post('/task', function(\Illuminate\Http\Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:10',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+	$task = new \App\Task;
+	$task->name = $request->name;
+	$task->save();
+
+	return redirect('/');
+});
+
+Route::delete('/task/{task}', function(\App\Task $task) {
+	$task->delete();
+
+	return redirect('/');
+});
